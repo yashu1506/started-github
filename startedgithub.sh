51d7070f6a931d457560ea108ce84951ed6754d9
yashu1506 <98607047+yashu1506@users.noreply.github.com>
2022-03-30 14:37:59 +0530

    Create startedgithub.sh

---
+++ startedgithub.sh
@@ -0,0 +1,15 @@
+          echo 1. what is in this directory..?
+          ls -a
+          echo 
+          echo 2. is java installed?
+          java -version
+          echo 3. is git installed?
+          git --version
+          echo 4. what is the wokrkspace location?
+          echo $RUNNER_WORKSPACE
+          echo 5. who is running this script?
+          whoami
+          echo 6. how is the disc laid out?
+          df
+          echo 7. what environment variables are available?
+          env
